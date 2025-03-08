dcfdbba41a908f5e1481481c2a43c7899994d1c0
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-08 10:09:22 -0300

    made a simple String validation for future use in the password management system

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,23 @@
+import 'dart:io';
+
 void main() {
-  print('hello world');
+  stdout.write('enter your first name: ');
+  String firstName = getStringInput();
+
+  stdout.write('enter your second name: ');
+  String secondName = getStringInput();
+
+  String name = firstName + ' ' + secondName;
+  print('your name is $name');
+}
+
+String getStringInput() {
+  String? input = stdin.readLineSync();
+
+  while (input == null || input.isEmpty) {
+    stdout.write('invalid input, try again: ');
+    input = stdin.readLineSync();
+  }
+
+  return input;
 }
