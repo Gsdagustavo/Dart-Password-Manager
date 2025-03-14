a046ca3d3ddbb558e1edb354c411cdd796ec918b
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-14 18:32:32 -0300

    transformed the input function into a static method of a class (Input class)

---
+++ lib/util/Input.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+// static class for input handling
+class Input {
+
+  static String getStringInput() {
+    String? input = stdin.readLineSync() ?? '';
+
+    while (input == null) {
+      stdout.write('Invalid input, try again: ');
+      input = stdin.readLineSync() ?? '';
+    }
+
+    while (input!.isEmpty) {
+      stdout.write('Invalid input, try again: ');
+      input = stdin.readLineSync() ?? '';
+    }
+
+    return input;
+  }
+}
